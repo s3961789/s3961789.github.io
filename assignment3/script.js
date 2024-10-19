d9796ed8c1d5e65a96ab367c24540bbfff0ea3ca
s3961789 <142763019+s3961789@users.noreply.github.com>
2024-10-19 16:19:07 +1100

    asg 3 updated

--- assignment3/script.js
+++ assignment3/script.js
@@ -20,58 +20,47 @@ const hoverImages = [
   "images/bali-hover.png",
   "images/ntt-hover.png",
 ];
+// HOVER: I found different ways to store the original images and hovered images
+// and decided to use two seperate arrays, because I'm most familiar with this methode
+// I tried using map, but there're some part that I'm not sure why we need it, so using
+// the methode that I'm most familiar with help me to deepen my understanding on it
 
 const islands = document.querySelectorAll(".island");
 let clickedState = new Array(islands.length).fill(false);
 
-islands.forEach((island, indexNo) => {
-  const originalSrc = island.src; // original image
-  const hoverSrc = originalSrc.replace(
-    originalImages[indexNo],
-    hoverImages[indexNo]
-  );
+for (let i = 0; i < islands.length; i++) {
+  const island = islands[i]; // Access the current island
+  const originalSrc = island.src; // Original image
+  const hoverSrc = originalSrc.replace(originalImages[i], hoverImages[i]); // Hover image replacement
 
-  // Handle click to toggle clicked state
-  island.addEventListener("click", () => {
-    clickedState[indexNo] = !clickedState[indexNo]; // Toggle state
-    island.src = clickedState[indexNo] ? hoverSrc : originalSrc; // Update image
-  });
-  // I decided to keep the hovered images stay when being clicked, so user will remember which island is being clicked
+  island.addEventListener("click", islandClicked);
 
-  // Change to hover image on mouseover (only if not clicked)
-  island.addEventListener("mouseover", () => {
-    if (!clickedState[indexNo]) {
+  function islandClicked() {
+    if (clickedState[i] === false) {
+      clickedState[i] = true; // island is being clicked
       island.src = hoverSrc;
-    }
-  });
-
-  // Revert to original image on mouseout (only if not clicked)
-  island.addEventListener("mouseout", () => {
-    if (!clickedState[indexNo]) {
+    } else {
+      clickedState[i] = false; // Reset state to not clicked
       island.src = originalSrc;
     }
-  });
-});
-// island.addEventListener("click", clickActive)
-// function clickActive (){
-//   clickedState[indexNo] = !clickedState[indexNo]; // Toggle state
-//   island.src = clickedState[indexNo] ? hoverSrc : originalSrc; // Update image
-// };
+  }
+  // I decided to keep the hovered images stay when being clicked, so user will remember which island is being clicked
+  // code explanation for myself: When the page loads, the state of each island is false by default (not clicked), so IF clickedstate = false -> make it clicked and ELSE clickedstate = true -> make it unclicked
 
-// island.addEventListener("mouseover", hoverActive);
-// function hoverActive() {
-//   island.src = hoverSrc;
-// }
+  island.addEventListener("mouseover", hoverActive);
+  function hoverActive() {
+    if (clickedState[i] === false) {
+      island.src = hoverSrc;
+    }
+  }
 
-// island.addEventListener("mouseout", hoverInactive);
-// function hoverInactive() {
-//   island.src = originalSrc;
-// }
-
-// HOVER: I found different ways to store the original images and hovered images
-// and decided to use two seperate arrays, because I'm most familiar with this methode
-// I tried using map, but there're somepart that I'm not sure why we need it, so using
-// the methode that I'm most familiar with help me to deepen my understanding on it
+  island.addEventListener("mouseout", hoverInactive);
+  function hoverInactive() {
+    if (clickedState[i] === false) {
+      island.src = originalSrc;
+    }
+  }
+}
 
 // CLICK = OPEN POPUP
 const popupData = {
@@ -128,8 +117,16 @@ const popup = document.getElementById("popup");
 const popupImage = document.getElementById("popup-image");
 const popupHeader = document.getElementById("popup-header");
 const popupText = document.getElementById("popup-text");
+const elements = document.querySelectorAll("[data-id]");
 
-// Function to open the popup with the relevant data
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", getData);
+  function getData(event) {
+    const id = event.currentTarget.getAttribute("data-id");
+    openPopup(id);
+  }
+}
+// Function to open the popup with the relevant data (header, picture, text, buttons)
 function openPopup(id) {
   const data = popupData[id];
   if (data) {
@@ -141,34 +138,22 @@ function openPopup(id) {
     popup.style.display = "flex";
     islands.forEach((island) => {
       island.style.pointerEvents = "none";
-      // this to make user can click at one island at a time only, to reduce confusion and distraction just in case they try to hover on the other area
+      // this make user can only click at one island at a time, to reduce confusion and distraction just in case they try to hover on the other area
       // to activate the hover interactions, they have to close the popup first
     });
   }
 }
 
-// Add click event listeners to elements with data-id
-document.querySelectorAll("[data-id]").forEach((element) => {
-  element.addEventListener("click", (event) => {
-    const id = event.currentTarget.getAttribute("data-id");
-    openPopup(id);
-  });
-});
-
 const closeButton = document.getElementById("close-button");
-
 closeButton.addEventListener("click", closePopup);
 
 function closePopup() {
   popup.style.display = "none";
 
   // Reset islands to their original state
-  islands.forEach((island, indexNo) => {
-    island.src = originalImages[indexNo]; // Reset to original image
-    clickedState[indexNo] = false; // Reset clicked state
-    island.style.pointerEvents = "auto"; // Re-enable interaction
-    // to ensure a smooth interaction within the interactive map, I make sure the images is being reset after the pop up is closed
-  });
+  for (let i = 0; i < islands.length; i++) {
+    islands[i].src = originalImages[i]; // Reset to original image
+    clickedState[i] = false; // Reset clicked state
+    islands[i].style.pointerEvents = "auto"; // Re-enable interaction
+  }
 }
-
-// logic: click on the sumatra island and open popup which contain image, header, and text
